e05b1aebd2b23df858f3515c06eaf8e128b6c7f7
Rajesh <rajinweb@gmail.com>
2025-11-16 23:22:34 +0530

    canvas issue

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Prevent AWS Amplify from trying to build node-canvas
+      config.resolve.alias["canvas"] = false;
+    }
+    return config;
+  },
   images: {
     remotePatterns: [
       {
